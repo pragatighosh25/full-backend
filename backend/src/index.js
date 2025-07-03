70eac3f68967a0b3e0161548624777d4176a9dd7
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-03 12:09:50 +0530

    db connection M2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
+//require("dotenv").config({path: './env'});
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: './env'
+})
 
 
+// Connect to MongoDB
+connectDB();
 
 
 
