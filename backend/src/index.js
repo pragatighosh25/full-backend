7482c7dae10a03f5f463c8983f1a9d766ab9ffbf
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-05 12:17:58 +0530

    server start

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,15 @@ dotenv.config({
 
 
 // Connect to MongoDB
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+  });
+})
+.catch(error => {
+  console.error("Error connecting to MongoDB:", error);
+});
 
 
 
