6a205b0f6894b2e6bdb51d1ff0f8938c969039dd
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-10 23:29:43 +0530

    middlewire multer

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import { response } from 'express';
+import fs from 'fs';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try{
+    if(!localFilePath) return null
+    // Upload the file to Cloudinary
+   const reponse= await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto'
+    })
+    //file has been uploaded successfully
+    console.log('File uploaded successfully:', reponse.url);
+    return response;
+  } catch (error){
+    fs.unlinkSync(localFilePath); // Delete the local file if upload fails
+    console.error('Error uploading file to Cloudinary:', error);
+    return null;
+  }
+}
+
+export default uploadOnCloudinary;
