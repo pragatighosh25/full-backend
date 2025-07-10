a2eabe7a2f252011f08aeb3b0161268052643918
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-10 22:32:04 +0530

    password ecryt

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -50,6 +50,13 @@ const userSchema = new Schema({
   timestamps: true,
 })
 
+userSchema.pre("save", async function(next){
+  if(!this.isModified('password')) return next(); 
+  this.password = await bcrypt.hash(this.password, 10);
+})
 
+userSchema.methods.isPasswordCorrect = async function(password) {
+  return await bcrypt.compare(password, this.password);
+} 
 
 export const User= mongoose.model('User', userSchema);
