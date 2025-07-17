8eef525256bb317f1ae6a0411163cd0aa3cb3c7c
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-17 12:03:25 +0530

    exsisting user & empty field

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import asyncHandler from '../utils/asyncHandler.js';
+import ApiError from '../utils/ApiError.js';
+import {User} from '../models/user.model.js'; 
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
@@ -14,7 +16,26 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const {fullname, email, username, password}= req.body;
   console.log("email:", email);
+
+  /*if(fullname=== ""){
+    throw new ApiError(400, "fullname is required")
+  }
+  */
+
+  if(
+    [fullname, email, password, username].some((field)=>field?.trim()=== "")
+  ){
+    throw new ApiError(400, "All fields are required")
+  }
   
+  const exsistedUser= User.findOne({
+    $or: [{username}, {email}]
+  })
+  if(exsistedUser){
+    throw new ApiError(409, "User with email or username already exsists")
+  }
+
+  req.files?.avatar[0]?.path
 })
 
 export default registerUser;
