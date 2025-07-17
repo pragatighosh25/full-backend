0f4e15e050d973290a022d82d94fd4c7f493d141
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-17 12:57:21 +0530

    send res

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from '../utils/asyncHandler.js';
 import ApiError from '../utils/ApiError.js';
 import {User} from '../models/user.model.js'; 
 import uploadOnCloudinary from '../utils/cloudinary.js'
+import ApiResponse from '../utils/ApiResponse.js';
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
@@ -65,6 +66,10 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!createdUser){
     throw new ApiError(500, "Error while creating user")
   }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "user registered successfully")
+  )
 })
 
 export default registerUser;
