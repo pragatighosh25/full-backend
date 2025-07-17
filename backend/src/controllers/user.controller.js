42fcdf66a533e7a59372986c91224a68efc06b84
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-17 12:44:12 +0530

    upload to cloudinary

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -42,6 +42,13 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required")
   }
+
+  const avatar= await uploadOnCloudinary(avatarLocalPath)
+  const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required")
+  }
 })
 
 export default registerUser;
